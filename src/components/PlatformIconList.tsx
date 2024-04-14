298f7a2ae5934b20d1ec8b2faa188808d52c9961
lloladin123 <blackiealexnader@yahoo.dk>
2024-04-14 15:12:33 +0200

    Added wrap="wrap" to Platformicons to stop overflow

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -30,7 +30,7 @@ const PlatformIconList = ({ platforms }: Props) => {
     web: BsGlobe,
   };
   return (
-    <HStack marginY={1}>
+    <HStack wrap="wrap" marginY={1}>
       {platforms.map((platform) => (
         <Icon as={iconMap[platform.slug]} color="gray.500" />
       ))}
